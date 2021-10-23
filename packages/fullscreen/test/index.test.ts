3e82ca6d5f315c698bda98ed58d4bf04ef343368
Alex Lohr <alex.lohr@logmein.com>
2021-10-23 23:02:26 +0200

    experiment: uvu testing

--- packages/fullscreen/test/index.test.ts
+++ packages/fullscreen/test/index.test.ts
@@ -1,56 +1,100 @@
-import { createRoot, createEffect, createSignal } from "solid-js";
-import { createFullscreen } from "../src";
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
 
-describe("createFullscreen", () => {
-  const ref = document.createElement("div");
+import { createRoot, createEffect, createSignal } from 'solid-js';
 
-  test("will call the fullscreen method", () =>
-    new Promise<void>(resolve => {
-      createRoot(dispose => {
-        const active = createFullscreen(ref);
-        const expected = [false, true];
-        createEffect(() => {
-          expect(active()).toBe(expected.shift());
-          if (expected.length === 0) {
-            dispose();
-            resolve();
-          }
-        });
-      });
-    }));
+import { createFullscreen } from '../src';
 
-  test("will exit fullscreen on reactive change", () =>
-    new Promise<void>(resolve => {
-      createRoot(dispose => {
-        const [fs, setFs] = createSignal(true);
-        const active = createFullscreen(ref, fs);
-        const expected = [false, true, false];
-        createEffect(() => {
-          expect(active()).toBe(expected.shift());
-          if (expected.length === 2) {
-            setTimeout(() => setFs(false), 50);
-          } else if (expected.length === 0) {
-            dispose();
-            resolve();
-          }
-        });
-      });
-    }));
+const fsTest = suite('createFullscreen', { ref: document.createElement('div') });
 
-  test("will open fullscreen with options", () =>
-    new Promise<void>(resolve => {
-      createRoot(dispose => {
-        const options: FullscreenOptions = { navigationUI: "hide" };
-        const active = createFullscreen(ref, undefined, options);
-        const expected = [false, true];
-        createEffect(() => {
-          expect(active()).toBe(expected.shift());
-          if (expected.length === 0) {
-            expect((window as any)._fullScreenOptions).toBe(options);
-            dispose();
-            resolve();
-          }
-        });
-      });
-    }));
+// mock fullscreen API
+fsTest.before(() => {
+  let currentFullscreenElement: HTMLElement | undefined;
+
+  HTMLElement.prototype.requestFullscreen = function (
+    this: HTMLElement,
+    options?: FullscreenOptions
+  ) {
+    (window as any)._fullScreenOptions = options;
+    currentFullscreenElement = this;
+    document.dispatchEvent(new Event("fullscreenchange"));
+    return Promise.resolve();
+  };
+  
+  Object.defineProperty(document, "fullscreenElement", {
+    value: currentFullscreenElement,
+    writable: false
+  });
+  
+  Object.defineProperty(document, "exitFullscreen", {
+    value: () => {
+      currentFullscreenElement = undefined;
+    },
+    writable: false
+  });  
 });
+
+fsTest('will call the fullscreen method', async ({ ref }) => {  
+  const expected = [false, true];
+  const actual: boolean[] = [];
+  
+  await new Promise<void>(resolve => createRoot(dispose => {
+    const active = createFullscreen(ref);
+
+    createEffect(() => {
+      actual.push(active());
+      if (actual.length === expected.length) {      
+        dispose()
+        resolve();        
+      }
+    });
+  }));
+
+  assert.equal(actual, expected);
+});
+
+fsTest("will exit fullscreen on reactive change", async ({ ref }) => {
+  const expected = [false, true, false];
+  const actual: boolean[] = [];
+
+  await new Promise<void>(resolve => createRoot(dispose => {
+    const [fs, setFs] = createSignal(true);
+    const active = createFullscreen(ref, fs);
+
+    createEffect(() => {
+      actual.push(active());
+      if (actual.length === 1) {
+        setTimeout(() => setFs(false), 50);
+      }
+      if (actual.length === expected.length) {      
+        dispose()
+        resolve();        
+      }
+    });
+  }));
+
+  assert.equal(actual, expected);
+});
+
+fsTest('will open fullscreen with options', async ({ ref }) => {  
+  const options: FullscreenOptions = { navigationUI: "hide" };
+  const expected = [false, true];
+  const actual: boolean[] = [];
+  
+  await new Promise<void>(resolve => createRoot(dispose => {
+    const active = createFullscreen(ref, undefined, options);
+
+    createEffect(() => {
+      actual.push(active());
+      if (actual.length === expected.length) {
+        dispose()
+        resolve();        
+      }
+    });
+  }));
+
+  assert.equal(actual, expected);
+  assert.equal((window as any)._fullScreenOptions, options, 'options are not used for fullscreen');
+});
+
+fsTest.run();
